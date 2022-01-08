d49a8d7c9e79a1a58ecea9d0f3e99676c08963e1
Mohamaddornaika <mohamaddornaika@gmail.com>
2022-01-09 00:34:04 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,18 +2,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+
+//const { clientId, guildId, } = require('./config.json');
+require('dotenv').config();
 
 const commands = [
     new SlashCommandBuilder().setName('madamnazar').setDescription('Replies with madam nazar location!'),
+    new SlashCommandBuilder().setName('snowman').setDescription('Wat snowman loves?'),
     new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
     new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
     .map(command => command.toJSON());
 // Create a new client instance
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.clientId, process.env.guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);

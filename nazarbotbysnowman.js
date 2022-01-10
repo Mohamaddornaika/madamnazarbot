48e72ec22cf87e3cb4b56182304156e8dbce4c15
Mohamaddornaika <mohamaddornaika@gmail.com>
2022-01-10 03:27:06 +0200

    Update nazarbotbysnowman.js

--- nazarbotbysnowman.js
+++ nazarbotbysnowman.js
@@ -19,6 +19,73 @@ client.on('interactionCreate', async interaction => {
 
 		interaction.reply(theurl)
 
+	}else if (commandName === 'FreeRoamEvent') {
+		//let obj = await fetch("https://madam-nazar-location-api.herokuapp.com/location/current");
+		//const myurl = await obj.json();
+
+		let obj = await fetch('https://api.rdo.gg/fme/');
+		let theurl = await obj.json();
+		var today = new Date();
+		let intlDateObj = new Intl.DateTimeFormat('en-GB', {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZone: "GMT"
+		});
+		var GMT = intlDateObj.format(today);
+		//var timeGMT = time.;
+		let theurlthemed = await theurl.themed;
+		let theurldefault = await theurl.default;
+		let mainurl = Object.assign(theurlthemed, theurldefault);
+		console.log(mainurl);
+		var keys = Object.keys(mainurl);
+		var EventTimeZero = Date.parse('01 Jan 1970 ' + keys[2] + ':00 GMT');
+		var TimeZero = Date.parse('01 Jan 1970 ' + GMT + ':00 GMT');
+		//console.log(EventTimeZero - TimeZero);
+		var Nextevent;
+		var timeleft = 0;
+		var count = 0;
+		for (x in mainurl) {
+			count++;
+		}
+		for (var i = 0; i < count; i++) {
+
+			var EventTime = Date.parse('01 Jan 1970 ' + keys[i] + ':00 GMT');
+
+			if ((EventTime - TimeZero) > 0) {
+
+				if (timeleft == 0) {
+					timeleft = (EventTime - TimeZero);
+					Nextevent = keys[i];
+				}
+				if ((EventTime - TimeZero) < timeleft) {
+					Nextevent = keys[i];
+					timeleft = (EventTime - TimeZero);
+					console.log(timeleft + " in if");
+				}
+			}
+		}
+		var timetime = new Date(timeleft);
+		// Minutes part from the timestamp
+		var minutes = " " + timetime.getMinutes();
+		// Seconds part from the timestamp
+		var seconds = " " + timetime.getSeconds();
+		console.log("there is " + minutes + " and " + seconds + " sec");
+		if (mainurl[Nextevent])
+
+			if (mainurl[Nextevent].variation) {
+				var variation = mainurl[Nextevent].variation.replaceAll('_', ' ')
+
+				var s2 = variation.substring(3);
+				await interaction.reply(s2 + " in " + minutes + " and " + seconds + " sec");
+			}
+			else {
+				var variation = mainurl[Nextevent].name.replaceAll('_', ' ')
+
+				var s2 = variation.substring(3);
+
+				await interaction.reply(s2 + " in " + minutes + " and " + seconds + " sec");
+			}
 	} else if (commandName === 'snowman') {
 		await interaction.reply('Snowman loves GG, the most go away lady!!!!!!');
 	} else if (commandName === 'Fam') {

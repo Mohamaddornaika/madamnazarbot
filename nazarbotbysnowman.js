9e64e76858351f5566a095a5aa7382cf98db9ac4
Mohamaddornaika <mohamaddornaika@gmail.com>
2022-01-10 18:26:35 +0200

    Update nazarbotbysnowman.js

--- nazarbotbysnowman.js
+++ nazarbotbysnowman.js
@@ -78,7 +78,14 @@ client.on('interactionCreate', async interaction => {
 				if (variation.includes("role")) {
 				var variation1 = mainurl[Nextevent].variation.replaceAll('role', '');
 					var s2 = variation1.substring(3);
-				await interaction.reply(s2 + " in " + minutes + " min and " + seconds + " sec");}
+				await interaction.reply(s2 + " in " + minutes + " min and " + seconds + " sec");
+				}
+				if (variation.includes("challenge")) {
+				
+				var variation1 = mainurl[Nextevent].variation.replaceAll('challenge', '');
+					var s2 = variation1.substring(3);
+				await interaction.reply(s2 + " in " + minutes + " min and " + seconds + " sec");
+				}
 				var s2 = variation.substring(3);
 				await interaction.reply(s2 + " in " + minutes + " min and " + seconds + " sec");
 			}
@@ -86,7 +93,7 @@ client.on('interactionCreate', async interaction => {
 				var variation = mainurl[Nextevent].name.replaceAll('_', ' ');
 				
 				if (variation.includes("role")) {
-				var variation1 = mainurl[Nextevent].variation.replaceAll('role', '');
+				var variation1 = mainurl[Nextevent].name.replaceAll('role', '');
 					var s2 = variation1.substring(3);
 				await interaction.reply(s2 + " in " + minutes + " min and " + seconds + " sec");}
 				var s2 = variation.substring(3);

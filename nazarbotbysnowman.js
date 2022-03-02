fa893621df51fe7fecd9d7bbca80ef3ef72235b6
Mohamaddornaika <mohamaddornaika@gmail.com>
2022-03-02 02:24:16 +0200

    Update nazarbotbysnowman.js

--- nazarbotbysnowman.js
+++ nazarbotbysnowman.js
@@ -1,4 +1,4 @@
-//this is uploaded it should 
+//this is uploaded it should  hopefully
 const { Client,  Intents } = require('discord.js');
 //require('dotenv').config();
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
